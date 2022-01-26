43a5f7631b2359d84661493fa1e8934a14465426
MaryamHafeezKhan <smarsalanshah125@gmail.com>
2022-01-26 20:41:16 +0500

    updated

---
+++ Lectures/Lecture04SwitchCase/Lecture04SwitchCase/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture04SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       char vowels = Convert.ToChar(Console.ReadLine());
+            switch (vowels)
+            {
+                case 'A' :
+                case 'a':
+                    {
+                        Console.WriteLine("its a vowel");
+                        break;
+                    }
+                case 'I':
+                case 'i':
+                    {
+                        Console.WriteLine("its a vowel");
+                        break;
+                    }
+                case 'O':
+                case 'o':
+                    {
+                        Console.WriteLine("its a vowel");
+                        break;
+                    }
+                case 'U':
+                case 'u':
+                    {
+                        Console.WriteLine("its a vowel");
+                        break;
+                    }
+                case 'E':
+                case 'e':
+                    {
+                        Console.WriteLine("its a vowel");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("its not a vowel");
+                        break;
+                    }
+            }
+
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
